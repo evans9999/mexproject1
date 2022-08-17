90a4dbfc63cfd1485410d7fc458549e436424d46
evans9999 <106432498+evans9999@users.noreply.github.com>
2022-08-17 19:48:39 +0200

    Update Roboter1.hpp

--- MEXPROJECT/mexproject1/mexproject1/MEXPROJECT/Roboter1.hpp
+++ MEXPROJECT/mexproject1/mexproject1/MEXPROJECT/Roboter1.hpp
@@ -6,13 +6,13 @@
 class Roboter1{
 public:
 
-  void move_servo_0(Pololu conn );
+  void move_servo_0(Pololu conn, bool Uhrzeigersinn );
 
-  void move_servo_1(Pololu conn);
+  void move_servo_1(Pololu conn, bool Uhrzeigersinn);
   
-  void move_servo_2(Pololu conn);
+  void move_servo_2(Pololu conn, bool Uhrzeigersinn);
   
-  void move_servo_3(Pololu conn);
+  void move_servo_3(Pololu conn,bool Uhrzeigersinn);
   
 
 };
