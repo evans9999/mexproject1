43d2183d124badb69777d26224fd323b1606adf4
evans9999 <106432498+evans9999@users.noreply.github.com>
2022-08-17 19:00:51 +0200

    Add files via upload

---
+++ MEXPROJECT/mexproject1/mexproject1/MEXPROJECT/Roboter3.hpp
@@ -0,0 +1,30 @@
+#ifndef ROBOTER3_HPP_INCLUDED
+#define ROBOTER3_HPP_INCLUDED
+
+
+
+class Roboter3{
+public:
+
+  void move_servo_0(Pololu conn );
+
+  void move_servo_1(Pololu conn);
+  
+  void move_servo_2(Pololu conn);
+  
+  void move_servo_3(Pololu conn);
+  
+
+};
+
+#endif /* SERVOMOTOR_HPP_ */
+
+
+
+
+
+
+
+
+
+
